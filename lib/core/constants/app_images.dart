211fe6efcf9825ab00eafebec7d9f24814b421db
abdelrahmankhaled0 <abdelrahmankhaledh@gmail.com>
2025-10-31 19:31:15 +0200

    final 2

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
-  static const String onboardingImage1 = "assets/images/amico2.png";
-  static const String onboardingImage2 = "assets/images/amico.png";
-  static const String onboardingImage3 = "assets/images/amico1.png";
+  static const String onboardingImage1 = "assets/images/on1.png";
+  static const String onboardingImage2 = "assets/images/on2.png";
+  static const String onboardingImage3 = "assets/images/on3.png";
   static const String welcomeImage = "assets/images/bro.png";
   static const String facebookImage = "assets/images/facebook.png";
   static const String googleImage = "assets/images/google.png";
